7089e141785904bc83ae9b077e3ebcd862004ca9
heritio <66847929+heritio@users.noreply.github.com>
2021-06-14 13:59:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/lib/printful-client.ts
@@ -0,0 +1,7 @@
+import { PrintfulClient } from "printful-request";
+
+export const printful = new PrintfulClient(
+  "ck6hnv8o-tsa1-at20:nmxz-59uddxbgvnfr"
+);
+
+//process.env.PRINTFUL_API_KEY
---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+function TermsPage() {
+  return (
+    <div className="max-w-xl mx-auto prose prose-blue">
+      <h2>Welcome</h2>
+      To our Store...
+    </div>
+  );
+}
+
+export default TermsPage;
